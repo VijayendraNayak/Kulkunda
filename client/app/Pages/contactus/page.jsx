41cbf722e4cda5cec233ee8194819f91ff03b73e
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2023-12-21 19:59:38 +0530

    final_Contact_us

--- client/app/Pages/contactus/page.jsx
+++ client/app/Pages/contactus/page.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 
-
 const Page = () => {
- 
+  const pic1 = {
+    backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3uRUc7n5bFxLqwBv1e-lnZeXP88KIFWQsrOZACC5EXw&s")`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '200px',
+    width: '100%',
+  };
+
+  const pic2 = {
+    backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3uRUc7n5bFxLqwBv1e-lnZeXP88KIFWQsrOZACC5EXw&s")`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '200px',
+    width: '100%',
+  };
+
+  const pic3 = {
+    backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3uRUc7n5bFxLqwBv1e-lnZeXP88KIFWQsrOZACC5EXw&s")`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '200px',
+    width: '100%',
+  };
+
+  //you can apply the background image to any of the perticular div by just calling style={pic1 or pic2 or pic3 } just replace the url link witht he actual link of the file 
 
   return (
     <div className="isolate mt-5 bg-gradient-to-r from-green-400 to-blue-500 text-gray-800">
 
       {/* Contact Information */}
-      <div className="mx-auto text-center bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mt-5">Contact Information</h2>
+      <div className="mx-auto text-center bg-white p-8 rounded-lg shadow-lg " >
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl mt-7">Contact Information</h2>
         <div className="mb-8">
           <h3 className="text-xl font-semibold text-gray-900 mb-3">Address</h3>
           <p>Shree Basaveshwara Temple,
@@ -48,40 +71,40 @@ const Page = () => {
 
         {/* Enquire Form */}
         <form action="#" method="POST" className="ml-12 mt-16 max-w-2xl sm:mt-7 bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-3xl font-bold tracking-tight text--900 sm:text-4xl">     Enquire form</h2>
-          <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-            <div class="sm:col-span-2">
-              <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Name</label>
-              <div class="mt-2.5">
-                <input type="text" name="first-name" id="text" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          <h2 className="text-3xl font-bold tracking-tight text--900 sm:text-4xl">Enquire form</h2>
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div className="sm:col-span-2">
+              <label htmlFor="name" className="block text-sm font-semibold leading-6 text-gray-900">Name</label>
+              <div className="mt-2.5">
+                <input type="text" name="name" id="name" autoComplete="given-name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
               </div>
             </div>
-            <div class="sm:col-span-2">
-              <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
-              <div class="mt-2.5">
-                <input type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <div className="sm:col-span-2">
+              <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+              <div className="mt-2.5">
+                <input type="email" name="email" id="email" autoComplete="email" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
               </div>
             </div>
-            <div class="sm:col-span-2">
-              <label for="phone-number" class="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
-              <div class="relative mt-2.5">
-                <input type="tel" name="phone-number" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <div className="sm:col-span-2">
+              <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
+              <div className="relative mt-2.5">
+                <input type="tel" name="phone-number" id="phone-number" autoComplete="tel" className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
               </div>
             </div>
-            <div class="sm:col-span-2">
-              <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Your Message</label>
-              <div class="mt-2.5">
-                <textarea name="message" id="message" rows="3" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            <div className="sm:col-span-2">
+              <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">Your Message</label>
+              <div className="mt-2.5">
+                <textarea name="message" id="message" rows="3" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
               </div>
             </div>
           </div>
-          <div class="mt-10">
-            <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
+          <div className="mt-10" >
+            <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
           </div>
         </form>
 
         {/* Google Map iframe */}
-        <div className='pl-12 ml-12 mt-6'>
+        <div className='ml-auto mt-7' >
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3892.3518917216397!2d75.6071434!3d12.690440299999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba4e6a6152c4c59%3A0x848a6ae907e47652!2sShree%20Basaveshwara%20Temple!5e0!3m2!1sen!2sin!4v1703072760786!5m2!1sen!2sin"
             width="400"
