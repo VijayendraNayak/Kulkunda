9bd9fcde9bc5f0cac1ff3dbe4abf642589bc1d77
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-12-25 10:55:08 +0530

    register error fix

--- client/app/Pages/register/page.jsx
+++ client/app/Pages/register/page.jsx
@@ -2,27 +2,22 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import Image from "next/image";
+import { IoIosEye } from "react-icons/io";
+import { IoIosEyeOff } from "react-icons/io";
 import RegisterImage from "/app/assets/image/temple.jpg";
-import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
-
-import {Icon} from 'react-icons-kit';
-import {eyeOff} from 'react-icons-kit/feather/eyeOff';
-import {eye} from 'react-icons-kit/feather/eye'
 
 const Register = () => {
   const [formdata, setFormdata] = useState({});
-  const search = useSearchParams();
-  const router = useRouter();
-  const phone = search.get("phone");
+  const [password, showPassword] = useState(true);
+  const router=useRouter()
 
-  const [passwordType, setPasswordType] = useState('text');
-  const [passwordIcon, setPasswordIcon] = useState(eyeOff);
-
-  const [confirmPasswordType, setConfirmPasswordType] = useState('text');
-  const [confirmPasswordIcon, setConfirmPasswordIcon] = useState(eyeOff);
-
-  const [icon, setIcon] = useState(eyeOff);
+  const handleChange = (e) => {
+    setFormdata({ ...formdata, [e.target.id]: e.target.value });
+  };
+  const togglepassword = () => {
+    showPassword(!password);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -44,35 +39,6 @@ const Register = () => {
       console.log("catcherr", error);
     }
   };
-
-  const handleChange = (e) => {
-    setFormdata({ ...formdata, [e.target.id]: e.target.value });
-  };
-
-
-  const togglepassword = () => {
-    // Toggle the password visibility (if needed)
-  };
-
-  const handlePasswordToggle = () => {
-    if (passwordType === 'password') {
-      setPasswordIcon(eye);
-      setPasswordType('text');
-    } else {
-      setPasswordIcon(eyeOff);
-      setPasswordType('password');
-    }
-  };
-
-  const handleConfirmPasswordToggle = () => {
-    if (confirmPasswordType === 'password') {
-      setConfirmPasswordIcon(eye);
-      setConfirmPasswordType('text');
-    } else {
-      setConfirmPasswordIcon(eyeOff);
-      setConfirmPasswordType('password');
-    }
-}
 
   return (
     <div className="pt-20 p-10 flex ">
@@ -106,7 +72,6 @@ const Register = () => {
               className="border p-3 rounded-lg hover:shadow-lg hover:scale-105"
               id="phonenumber"
               onChange={handleChange}
-              defaultValue={phone}
             />
             <input
               type="email"
@@ -117,31 +82,35 @@ const Register = () => {
             />
             <div className="relative">
               <input
-                  type={passwordType}
-                  id="password"
-                  placeholder="Password"
-                  className="border p-3 rounded-lg pr-10 w-[245px] sm:w-[350px] hover:shadow-lg hover:scale-105"
-                  value={formdata.password}
-                  onChange={(e) => setPasswordType(e.target.value)}
-                  autoComplete="current-password"
+                type={password ? "password" : "text"}
+                placeholder="Password"
+                className="border p-3 rounded-lg pr-10 w-[245px] sm:w-[350px] hover:shadow-lg hover:scale-105"
+                id="password"
+                onChange={handleChange}
               />
-              <span className="flex justify-around items-center" onClick={handlePasswordToggle}>
-                <Icon className="absolute top-1/2 left-80 transform -translate-y-1/2 hover:shadow-lg hover:scale-105" icon={passwordIcon} size={25}/>
-              </span>
+              <button
+                type="button"
+                onClick={togglepassword}
+                className="absolute top-1/2 left-80 transform -translate-y-1/2 hover:shadow-lg hover:scale-105"
+              >
+                {password ? <IoIosEye /> : <IoIosEyeOff />}
+              </button>
             </div>
             <div className="relative">
               <input
-                  type={confirmPasswordType}
-                  id="confirm-password"
-                  placeholder="Confirm-Password"
-                  className="border p-3 rounded-lg pr-10 w-[245px] sm:w-[350px] hover:shadow-lg hover:scale-105"
-                  value={formdata.confirmpassword}
-                  onChange={(e) => setConfirmPasswordType(e.target.value)}
-                  autoComplete="current-password"
+                type={password ? "password" : "text"}
+                placeholder="Confirm Password"
+                className="border p-3 rounded-lg pr-10 w-[245px] sm:w-[350px] hover:shadow-lg hover:scale-105"
+                id="confirmpassword"
+                onChange={handleChange}
               />
-              <span className="flex justify-around items-center" onClick={handleConfirmPasswordToggle}>
-                <Icon className="absolute top-1/2 left-80 transform -translate-y-1/2 hover:shadow-lg hover:scale-105" icon={confirmPasswordIcon} size={25}/>
-              </span>
+              <button
+                type="button"
+                onClick={togglepassword}
+                className="absolute top-1/2 left-80 transform -translate-y-1/2 hover:shadow-lg hover:scale-105"
+              >
+                {password ? <IoIosEye /> : <IoIosEyeOff />}
+              </button>
             </div>
           </form>
           <button
