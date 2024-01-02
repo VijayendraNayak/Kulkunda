c103428f991f2a89bbfa212cdd591fa8e90b61d8
chandan <chandanb200317@gmail.com>
2024-01-02 22:14:06 +0530

    home page

---
+++ client/app/Components/ImageSlider.js
@@ -0,0 +1,60 @@
+// ImageSlider.js
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Slide1 from "/app/assets/image/lake.jpg";
+import Slide2 from "/app/assets/image/ship.webp";
+
+const images = [Slide1, Slide2];
+
+const ImageSlider = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setCurrentPage((prev) => (prev + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handlePrevPage = () => {
+    setCurrentPage((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prev) => (prev + 1) % images.length);
+  };
+
+  return (
+    <div className='relative flex h-[calc(100vh-70px)] w-screen flex-col justify-center items-center'>
+      <div className='relative w-full h-[calc(100vh-70px)] flex items-center justify-center overflow-hidden'>
+        {/* Image Slider */}
+        <div className='relative w-full h-full'>
+          <Image
+            className='w-full h-full object-cover object-center'
+            sizes='100vw'
+            fill
+            alt={`Image ${currentPage + 1}`}
+            src={images[currentPage]}
+          />
+          <div className='absolute bottom-8 left-10 py-3 px-6 bg-black rounded-lg'>
+            <h2 className='text-4xl text-white'>Temple {currentPage + 1}</h2>
+            <p className='text-2xl mt-4 text-purple-200'>Peace</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Left arrow */}
+      <div onClick={handlePrevPage} className='z-10 absolute text-white bottom-1/2 left-4 text-2xl  md:left-8 font-semibold cursor-pointer hover:text-blue-500'>
+        <span className='inline-block transition-transform duration-300 ease-in-out transform hover:-translate-x-1'>&lt;-</span>
+      </div>
+
+      {/* Right arrow */}
+      <div onClick={handleNextPage} className='z-10 absolute bottom-1/2 text-white right-8 text-2xl  md:right-8 font-semibold cursor-pointer hover:text-blue-500'>
+        <span className='inline-block transition-transform duration-300 ease-in-out transform hover:translate-x-1'>-&gt;</span>
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
--- client/app/page.js
+++ client/app/page.js
@@ -1,13 +1,92 @@
+"use client"
+import React from 'react';
+import Link from 'next/link.js';
+import ImageSlider from './Components/ImageSlider.js';
+import Image from 'next/image';
+import Temple from "/app/assets/image/temple.jpg";
 
-
-import React from 'react'
-
-const page = () => {
+const Page = () => {
   return (
     <div>
-      hello
-    </div>
-  )
-}
+      {/* Image Slider */}
+      <ImageSlider />
 
-export default page
+      <div className='flex flex-col md:flex-row p-5 md:p-5'>
+        {/* Image Section */}
+        <div className='w-full md:w-1/2'>
+          <Image
+            src={Temple}
+            className='ml-20 shadow-lg mt-3'
+            alt="register background image"
+            // objectFit="cover"
+            width={500}
+            height={500}
+            objectPosition="left"
+            priority
+          />
+        </div>
+
+        {/* Data Section */}
+        <div className='flex-1 bg-ora p-5 hover:scale-105 mr-10'>
+          <div className='bg-white border-2 border-black p-4 rounded-lg shadow-md'>
+            <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
+            <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
+            <Link href="/Pages/About">
+              <p className='text-red-500'>Read more.........</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className='flex-1 bg-ora p-5 hover:scale-105 mr-10'>
+        <div className='bg-white border-2 border-black p-4 rounded-lg shadow-md'>
+          <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
+          <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
+          <Link href="/Pages/About">
+            <p className='text-red-500'>Read more......</p>
+          </Link>
+        </div>
+      </div>
+
+      <div className='relative flex items-center justify-center'>
+          <Image
+            src={Temple}
+            className='ml-20 shadow-lg mt-3'
+            alt="register background image"
+            objectFit="cover"
+            width={500}
+            height={500}
+            priority
+          />
+        </div>
+
+
+      <h1 className='text-center bold text-5xl text-black-400 mt-10 underline'>ABOUT THE OWNERS</h1>
+      <div className='flex flex-col md:flex-row p-5 md:p-10'>
+        {/* Image Section */}
+        <div className='flex-1 bg-ora p-10 hover:scale-105 mr-10'>
+          <div className='bg-white border-2 border-black p-4 rounded-lg shadow-md'>
+            <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
+            <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
+          </div>
+        </div>
+
+        <div className='flex-1 bg-ora p-10 hover:scale-105 mr-10'>
+          <div className='bg-white border-2 border-black p-4 rounded-lg shadow-md'>
+            <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
+            <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
+          </div>
+        </div>
+
+        {/* Data Section */}
+        <div className='flex-1 bg-ora p-10 hover:scale-105 mr-10'>
+          <div className='bg-white border-2 border-black p-4 rounded-lg shadow-md'>
+            <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
+            <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
