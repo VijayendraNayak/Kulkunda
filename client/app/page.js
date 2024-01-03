b9709404da5e6be432b08bd3bec8c9a59830aaae
chandan <chandanb200317@gmail.com>
2024-01-03 12:14:16 +0530

    home slider

--- client/app/page.js
+++ client/app/page.js
@@ -4,6 +4,8 @@ import Link from 'next/link.js';
 import ImageSlider from './Components/ImageSlider.js';
 import Image from 'next/image';
 import Temple from "/app/assets/image/temple.jpg";
+import Temple2 from "/app/assets/image/temple2.jpg";
+
 
 const Page = () => {
   return (
@@ -19,11 +21,51 @@ const Page = () => {
             className='ml-20 shadow-lg mt-3'
             alt="register background image"
             // objectFit="cover"
-            width={500}
-            height={500}
+            width={300}
+            height={300}
             objectPosition="left"
             priority
           />
+        </div>
+        {/* Data Section */}
+        <div className='flex-1 bg-ora p-5 hover:scale-105 mr-20'>
+          <div className='bg-white border-2 border-black p-4 rounded-lg shadow-md'>
+            <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
+            <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
+            <Link href="/Pages/About">
+              <p className='text-red-500'>Read more.........</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className='relative flex items-center justify-center'>
+        <div className='flex-1 bg-ora p-5 hover:scale-105 ml-10'>
+          <div className='bg-white border-2 border-black p-4 rounded-lg shadow-md'>
+            <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
+            <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
+            <Link href="/Pages/About">
+              <p className='text-red-500'>Read more.........</p>
+            </Link>
+          </div>
+        </div>
+        <Image
+          src={Temple2}
+          className='mr-10 shadow-lg mt-3 relative'
+          alt="register background image"
+          objectFit="cover"
+          width={200}
+          height={200}
+          priority
+        />
+      </div>
+
+
+      {/* slide news */}
+      <div className='flex flex-col md:flex-row p-5 md:p-5'>
+        {/* Image Section */}
+        <div className='w-full md:w-1/2'>
+          
         </div>
 
         {/* Data Section */}
@@ -37,27 +79,10 @@ const Page = () => {
           </div>
         </div>
       </div>
-      <div className='flex-1 bg-ora p-5 hover:scale-105 mr-10'>
-        <div className='bg-white border-2 border-black p-4 rounded-lg shadow-md'>
-          <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
-          <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
-          <Link href="/Pages/About">
-            <p className='text-red-500'>Read more......</p>
-          </Link>
-        </div>
-      </div>
 
-      <div className='relative flex items-center justify-center'>
-          <Image
-            src={Temple}
-            className='ml-20 shadow-lg mt-3'
-            alt="register background image"
-            objectFit="cover"
-            width={500}
-            height={500}
-            priority
-          />
-        </div>
+
+
+
 
 
       <h1 className='text-center bold text-5xl text-black-400 mt-10 underline'>ABOUT THE OWNERS</h1>
