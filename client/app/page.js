67f531b16d090637162c82c019960b6351d1684a
chandan <chandanb200317@gmail.com>
2024-01-11 18:49:21 +0530

    gallery page

--- client/app/page.js
+++ client/app/page.js
@@ -5,6 +5,7 @@ import ImageSlider from './Components/ImageSlider.js';
 import Image from 'next/image';
 import Temple from "/app/assets/image/temple.jpg";
 import Temple2 from "/app/assets/image/temple2.jpg";
+import G1 from "/app/assets/image/entrance.jpg";
 import MarqueeComponent from './Components/MarqueeComponent';
 import Owner from './Components/OwnerSection.js'
 
@@ -56,7 +57,7 @@ const Page = () => {
                 priority
               />
               <Image
-                src={Temple2}
+                src={G1}
                 className='mt-3 mr-3 p-5'
                 alt="gallery image"
                 objectFit="cover"
@@ -83,6 +84,11 @@ const Page = () => {
                 priority
               />
             </div>
+            <Link href="/Pages/gallery">
+              <p className="text-red-500 font-bold cursor-pointer hover:scale-110 hover:underline">
+                For more photos? Click Here....
+              </p>
+            </Link>
           </div>
         </div>
         <div className='w-full md:w-1/3 mr-20 mt-20'>
@@ -99,7 +105,7 @@ const Page = () => {
         {/* Image Section */}
         
         {/* Data Section */}
-        <div className='flex-1 bg-ora p-5 hover:scale-105 mr-10'>
+        <div className='flex-1 bg-ora p-5 mr-10'>
           <div className='bg-orange-100 border-2 border-orange-500 p-4 rounded-lg shadow-md'>
             <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
             <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
