31bc50871058bfcdca74987006d949f6411319c0
Abhi-gowda7 <abhishekgouda427@gmail.com>
2024-01-14 01:47:12 +0530

    message

--- client/app/Pages/seva/page.jsx
+++ client/app/Pages/seva/page.jsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import Entrance from "/app/assets/image/entrance.jpg";
 import Loader from "../../Components/Loader";
 
+
 const SevaPage = () => {
   const [language, setLanguage] = useState("english");
   const [formdata, setFormdata] = useState(null);
--- client/app/page.js
+++ client/app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link.js';
 import ImageSlider from './Components/ImageSlider.js';
 import Image from 'next/image';
@@ -11,7 +11,14 @@ import Owner from './Components/OwnerSection.js'
 
 
 const Page = () => {
-  return (
+  const [language, setLanguage] = useState('english');
+
+  const handleLanguageChange = (selectedLanguage) => {
+    setLanguage(selectedLanguage);
+    // Implement logic for language change as needed
+    // You can use this function to update the content based on the selected language.
+  };
+    return (
     <div classname='container mx-auto'>
       {/* Image Slider */}
       <ImageSlider />
@@ -34,16 +41,28 @@ const Page = () => {
         <div className='flex-1 bg-ora p-5 md:ml-5 lg:ml-10 hover:scale-105 md:mr-5'>
           <div className='bg-orange-100 border-2 border-orange-500 p-8 rounded-lg shadow-md mr-5'>
             <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
-            <p className='text-gray-700 line-clamp-3 md:line-clamp-5'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
+            <p className='text-gray-700 line-clamp-3 md:line-clamp-5'>About 800 years ago, during the rule of Keladi kings ShivappaNayaka and Surappa Nayaka in Kadambasamsthan (Kadaba), thesignificance of Basaveshwara and Kulkunda's history wasacknowledged by the royal family. Despite facing adversities suchas a smallpox epidemic, the devotion to Lord Basaveshwara endured,with devotees constructing a statue for worship. </p>
             <Link href="/Pages/About">
               <p className='text-red-500'>Read more.........</p>
             </Link>
           </div>
         </div>
+        <div className="flex justify-end pr-8 mb-4">
+        <label className="text-lg mr-2">Select Language:</label>
+        <select
+          className="border p-1"
+          value={language}
+          onChange={(e) => handleLanguageChange(e.target.value)}
+        >
+          <option value="english">English</option>
+          <option value="kannada">ಕನ್ನಡ</option>
+          
+        </select>
       </div>
+              </div>
 
       <div className='flex flex-col md:flex-row md:p-5'>
-        <div className='w-full md:w-2/3 lg:ml-10'>
+        <div className='w-full md:w-2/3 lg:ml-5'>
           
 
 
@@ -65,21 +84,32 @@ const Page = () => {
         <div className='flex-1 bg-ora p-5 md:mr-10'>
           <div className='bg-orange-100 border-2 border-orange-500 p-4 rounded-lg shadow-md'>
             <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
-            <p className='text-gray-700'>What is Lorem Ipsum?Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </p>
+            <p className='text-gray-700'> It's believed that visiting Lord Kukke Subrahmanya and thenseeking darshan of Lord Sri Basaveshwara (Nandishwara) whileoffering prayers brings complete fulfillment for devotees' wishes.Devotees firmly believe that Lord Basaveshwara resides atBasavanmoola and grants the sincere desires of his devotees., </p>
             <Link href="/Pages/About">
               <p className='text-red-500'>Read more.........</p>
             </Link>
           </div>
         </div>
+        {/*kannada*/ }
+        <div className='flex-1 bg-ora p-5 md:mr-10'>
+          <div className='bg-orange-100 border-2 border-orange-500 p-4 rounded-lg shadow-md'>
+            <h3 className='text-xl font-semibold mb-2'>Shri Basaveshwara Temple</h3>
+            <p className='text-gray-700'> ಸುಮಾರು 800 ವರ್ಷಗಳ ಹಿಂದೆ ಕೆಳದಿ ರಾಜರಾದ ಶಿವಪ್ಪ ನಾಯಕ, ಸೂರಪ್ಪ ನಾಯಕ ಎಂಬರಾಜರುಗಳು "ಕದಂಬಸಂಸ್ಥಾನ" (ಕಡಬ)ವನ್ನು ಆಳುತ್ತಿದ್ದರು ಎಂಬುದು ಇತಿಹಾಸದಲ್ಲಿತಿಳಿದು ಬರುತ್ತದೆ. ಅವರುಗಳು ಶ್ರೀ ಬಸವನ ಮೂಲ ಹಾಗೂ ಕುಲ್ಕುಂದ ಇತಿಹಾಸವನ್ನುತಿಳಿದು ಹಣೆಯಲ್ಲಿ ಶಿವಲಿಂಗವಿರುವ ಬಸವನನ್ನು ಕಂಡು ಪ್ರೇರೇಪಿತರಾಗಿ ಅವರಕಾಲಘಟ್ಟದಲ್ಲಿಯೇ ಪ್ರತಿಷ್ಠಾಪಿಸಿದ್ದರು ಎಂಬುದು ಕೂಡ ಅಷ್ಟಮಂಗಲ ಚಿಂತನೆಯಲ್ಲಿತಿಳಿದು ಬಂದಿರುತ್ತದೆ. ಆನಂತರದ ವರ್ಷಗಳಲ್ಲಿ ಸಿಡುಬು ರೋಗ ಮಹಾಮಾರಿಯಿಂದಸಂಪೂರ್ಣ ಕುಕ್ಕೆ ಪಟ್ಟಣ ನಾಶ ಹೊಂದಿತು. </p>
+            <Link href="/Pages/About">
+              <p className='text-red-500'>Read more.........</p>
+            </Link>
+          </div>
+
       </div>
 
-
+      
       <h1 className='text-center bold text-5xl text-black-400 underline '>ABOUT THE OWNERS</h1>
       <div className='flex flex-col md:flex-row md:p-10'>
         <Owner/>
       </div>
     </div>
-  );
+    </div>
+      );
 };
 
 export default Page;
