10708f30f5ec1bfed96cc83ac684456cc477ca87
Sudheesh Patwardhan <sudheesh.is21@gmail.com>
2024-01-13 21:29:29 +0530

    display all booked sevas

--- server/models/sevaModel.js
+++ server/models/sevaModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const sevaSchema = new mongoose.Schema({
   sevaname: { type: String, required: true },
   username: { type: String, required: true }, // New field
-  sevadate: { type: Date, required: true },
   phonenumber: { type: Number, required: true },
+  sevadate: { type: Date, required: true },
   userId: { type: String, required: true },
 });
 
